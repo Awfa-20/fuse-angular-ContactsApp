14529f94bfb870b19cacdc5c99b5f442d38eb429
Aufa alkarfan <awfa.alkrfan@gmail.com>
2022-02-25 15:07:38 +0300

    adding filter to contact list

--- src/app/main/contacts/contacts-list/contacts-list.component.ts
+++ src/app/main/contacts/contacts-list/contacts-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { MatTableDataSource } from '@angular/material/table';
 import { SelectionModel, DataSource } from '@angular/cdk/collections';
 import { ContactsDto } from '../contactsDto';
@@ -17,96 +17,109 @@ import { Subscription } from 'rxjs';
 //     {id: 7, first_Name: 'Can', last_Name: 'Van', company: 'ITM', job_Title: 'Junior', email: 'can@gmail.com', phone: '05556668899', notes: ''},
 //     {id: 8, first_Name: 'Can', last_Name: 'Van', company: 'ITM', job_Title: 'Junior', email: 'can@gmail.com', phone: '05556668899', notes: ''}
 //   ];
-  
 
 @Component({
-  selector: 'app-contacts-list',
-  templateUrl: './contacts-list.component.html',
-  styleUrls: ['./contacts-list.component.scss']
+    selector: 'app-contacts-list',
+    templateUrl: './contacts-list.component.html',
+    styleUrls: ['./contacts-list.component.scss'],
 })
 export class ContactsListComponent implements OnInit, OnDestroy {
-
-    displayedColumns = ['select', 'imagePath', 'full_Name', 'email', 'phone', 'jobTitle&Company', 'edit', 'delete'];
-    dataSource = this.contactsService.getContacts();
-    subscription: Subscription;
-    contacts: ContactsDto[];
+    displayedColumns = [
+        'select',
+        'imagePath',
+        'full_Name',
+        'email',
+        'phone',
+        'jobTitle&Company',
+        'edit',
+        'delete',
+    ];
+    // dataSource = this.contactsService.getContacts();
     
+    subscription: Subscription;
+    contacts: ContactsDto[] =  this.contactsService.getContacts();
+    dataSource = new MatTableDataSource(this.contacts);
 
     /** Selects all rows if they are not all selected; otherwise clear selection. */
     allowMultiSelect = true;
     selection: SelectionModel<ContactsDto>;
-  
-    constructor(private contactsService: ContactsService, private router: Router, private route: ActivatedRoute) {
-      this.selection = new SelectionModel<ContactsDto>(this.allowMultiSelect, []);
+
+    constructor(
+        private contactsService: ContactsService,
+        private router: Router,
+        private route: ActivatedRoute
+    ) {
+        this.selection = new SelectionModel<ContactsDto>(
+            this.allowMultiSelect,
+            []
+        );
     }
 
     ngOnInit(): void {
-        this.subscription = this.contactsService.contactChanged
-        .subscribe((contacts: ContactsDto[]) => {
-            this.contacts = contacts;
-        });
+        this.subscription = this.contactsService.contactChanged.subscribe(
+            (contacts: ContactsDto[]) => {
+                this.contacts = contacts;
+                this.dataSource = new MatTableDataSource(contacts);
+            }
+        );
         this.contacts = this.contactsService.getContacts();
     }
 
     onNewContact() {
-        this.router.navigate(['../contact/new'], {relativeTo: this.route});
-      }
+        this.router.navigate(['../contact/new'], { relativeTo: this.route });
+    }
 
-    onEditContact(contact: ContactsDto){
+    onEditContact(contact: ContactsDto) {
         const id = this.contacts.indexOf(contact) + 1;
-        this.router.navigate(['../contact/edit'  , id], {relativeTo: this.route});
+        this.router.navigate(['../contact/edit', id], {
+            relativeTo: this.route,
+        });
     }
 
-    onDeleteContact(contact: ContactsDto){
-    
-    this.contactsService.deleteContact(contact);
+    onDeleteContact(contact: ContactsDto) {
+        this.contactsService.deleteContact(contact);
     }
 
-    getInfo(contact: ContactsDto){
+    getInfo(contact: ContactsDto) {
         const id = this.contacts.indexOf(contact) + 1;
-        this.router.navigate(['../contact'  , id], {relativeTo: this.route});
-
+        this.router.navigate(['../contact', id], { relativeTo: this.route });
     }
-  
+
     masterToggle() {
-      if (this.isAllSelected()) {
-        this.selection.clear();
-        return;
-      }
-  
-      this.selection.select(...this.contacts);
+        if (this.isAllSelected()) {
+            this.selection.clear();
+            return;
+        }
+
+        this.selection.select(...this.contacts);
     }
-  
+
     /** The label for the checkbox on the passed row */
     checkboxLabel(row?: ContactsDto): string {
-      if (!row) {
-        return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
-      }
-      return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.phone}`;
+        if (!row) {
+            return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+        }
+        return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
+            row.phone
+        }`;
     }
-  
+
     isAllSelected() {
-      const numSelected = this.selection.selected.length;
-      if (this.dataSource) {
-        const numRows = this.contacts.length;
-        // tslint:disable-next-line: triple-equals
-        return numSelected == numRows;
-      }
-      return false;
+        const numSelected = this.selection.selected.length;
+        if (this.dataSource) {
+            const numRows = this.contacts.length;
+            // tslint:disable-next-line: triple-equals
+            return numSelected == numRows;
+        }
+        return false;
     }
 
-  
+    applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
 
-  applyFilter(event: Event) {
-    // const filterValue = (event.target as HTMLInputElement).value;
-    // this.schoolParams.school_Name = filterValue;
-    // this.schoolParams.phone = filterValue;
-    // this.loadSchools();
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
-
-
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 }
